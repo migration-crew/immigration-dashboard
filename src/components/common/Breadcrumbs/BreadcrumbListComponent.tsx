a2cc895ad7370c871dcf933f0bc27a8d0c2d1a46
David Torrez <dtorrez1996@gmail.com>
2024-11-18 17:29:33 -0500

    Create BreadcrumbListComponent.tsx

---
+++ src/components/common/Breadcrumbs/BreadcrumbListComponent.tsx
@@ -0,0 +1,40 @@
+import {
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Link } from "@/types/breadcrumb-link";
+import React from "react";
+import { HeadingSemi } from "../text/HeadingSemi";
+
+type Props = {
+  links: Link[];
+};
+
+export function BreadcrumbListComponent({ links }: Props) {
+  return (
+    <BreadcrumbList>
+      {links.map((link, index) => {
+        const isLast = index === links.length - 1;
+        return (
+          <React.Fragment key={link.href}>
+            <BreadcrumbItem>
+              {isLast ? (
+                <BreadcrumbPage className="text-primary-red">
+                  <HeadingSemi>{link.name}</HeadingSemi>
+                </BreadcrumbPage>
+              ) : (
+                <BreadcrumbLink href={link.href}>
+                  <HeadingSemi>{link.name}</HeadingSemi>
+                </BreadcrumbLink>
+              )}
+            </BreadcrumbItem>
+            {!isLast && <BreadcrumbSeparator />}
+          </React.Fragment>
+        );
+      })}
+    </BreadcrumbList>
+  );
+}
