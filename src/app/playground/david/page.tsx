c867998fa729d86215b53efe99feaae78eca1f5a
David Torrez <dtorrez1996@gmail.com>
2024-11-18 17:35:24 -0500

    test Breadcrumb in /playground/david

--- src/app/playground/david/page.tsx
+++ src/app/playground/david/page.tsx
@@ -1,13 +1,20 @@
+import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
 import { DynamicRoundedContainer } from "@/components/common/DynamicRoundedContainer";
 import { PageContainer } from "@/components/common/PageContainer";
 import { ParagraphRegular } from "@/components/common/text/ParagraphRegular";
 
 export default function DavidPage() {
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "Playground", href: "/playground" },
+    { name: "David", href: "/playground/david" },
+  ];
   return (
     <PageContainer>
       <h1 className="text-center text-blue-950 text-2xl font-bold m-3 ">
         David&apos;s playground
       </h1>
+      <BreadcrumbComponent links={links} />
       <DynamicRoundedContainer
         title="This is a dynamic rounded container"
         className="flex flex-col gap-5"
