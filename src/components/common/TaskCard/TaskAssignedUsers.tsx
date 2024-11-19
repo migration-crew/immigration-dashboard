2e0340a3ef7433fae39069f0b38eeb4016cd88d4
David Torrez <dtorrez1996@gmail.com>
2024-11-18 23:33:24 -0500

    Create TaskAssignedUsers component

---
+++ src/components/common/TaskCard/TaskAssignedUsers.tsx
@@ -0,0 +1,21 @@
+import { user } from "@/types/user";
+import { Avatar, AvatarFallback, AvatarImage } from "../../ui/avatar";
+
+type Props = {
+  assignedUsers: user[];
+};
+
+export function TaskAssignedUsers({ assignedUsers }: Props) {
+  return (
+    <ul className="flex gap-1 items-center">
+      {assignedUsers.map((user) => (
+        <li key={user._id}>
+          <Avatar>
+            <AvatarImage src={user.avatar} alt={user.name} />
+            <AvatarFallback>{user.name}</AvatarFallback>
+          </Avatar>
+        </li>
+      ))}
+    </ul>
+  );
+}
