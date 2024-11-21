4037154710f1649ce5f5c4112d20fb10b1977b60
YuiKonaka <youthfuldays585@gmail.com>
2024-11-21 15:24:03 -0800

    Completed DynamicHeaderContainer

--- src/app/playground/yui/page.tsx
+++ src/app/playground/yui/page.tsx
@@ -1,7 +1,4 @@
-import { Applicationtable } from "@/components/common/ApplicationTable";
 import DynamicHeaderContainer from "@/components/common/DynamicHeaderContainer";
-import HoraizontalProgressBar from "@/components/common/HoraizontalProgressBar";
-import { StatusTag } from "@/components/common/StatusTag";
 import { CaptionSemi } from "@/components/common/text/CaptionSemi";
 
 const page = () => {
@@ -9,37 +6,8 @@ const page = () => {
     <div>
       <DynamicHeaderContainer
         headerChildren={<CaptionSemi>Visa Application</CaptionSemi>}
-        contentChildren={
-          <>
-            <>
-              <HoraizontalProgressBar progress={50} />
-            </>
-            <>
-              <StatusTag progress={30} />
-            </>
-          </>
-        }
+        contentChildren={"Hello"}
       />
-      <>
-        <Applicationtable
-          applicationData={[
-            {
-              number: "001",
-              name: "Application One",
-              type: "Type A",
-              progress: "50%",
-              status: "In Progress",
-            },
-            {
-              number: "002",
-              name: "Application Two",
-              type: "Type B",
-              progress: "75%",
-              status: "In Progress",
-            },
-          ]}
-        />
-      </>
     </div>
   );
 };
