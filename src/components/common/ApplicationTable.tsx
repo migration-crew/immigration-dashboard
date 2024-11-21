8f1d66924c442439f48deecf326d053b8e8c33df
YuiKonaka <youthfuldays585@gmail.com>
2024-11-21 14:54:45 -0800

    Modified  ApplicationTable

--- src/components/common/ApplicationTable.tsx
+++ src/components/common/ApplicationTable.tsx
@@ -23,19 +23,31 @@ export function Applicationtable({ applicationData }: Props) {
   return (
     <Table className="w-[1152px] h-[49px] rounded-lg">
       <TableHeader className="bg-secondary-medium-gray text-primary-white rounded-lg">
-        <TableRow className="text-primary-white">
-          <TableHead className="text-primary-white">NUMBER</TableHead>
-          <TableHead className="text-primary-white">NAME</TableHead>
-          <TableHead className="text-primary-white">TYPE</TableHead>
-          <TableHead className="text-primary-white">PROGRESS</TableHead>
-          <TableHead className="text-primary-white">STATUS</TableHead>
+        <TableRow className="h-[21px] text-primary-white">
+          <TableHead className="text-primary-white text-caption">
+            NUMBER
+          </TableHead>
+          <TableHead className="text-primary-white text-caption">
+            NAME
+          </TableHead>
+          <TableHead className="text-primary-white  text-caption">
+            TYPE
+          </TableHead>
+          <TableHead className="text-primary-white  text-caption">
+            PROGRESS
+          </TableHead>
+          <TableHead className="text-primary-white  text-caption">
+            STATUS
+          </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {applicationData.map((application, index) => (
           <TableRow key={index}>
-            <TableCell className="font-medium">{application.number}</TableCell>
-            <TableCell>{application.name}</TableCell>
+            <TableCell>{application.number}</TableCell>
+            <TableCell className="text-caption-semi">
+              {application.name}
+            </TableCell>
             <TableCell>{application.type}</TableCell>
             <TableCell>{application.progress}</TableCell>
             <TableCell className="">{application.status}</TableCell>
