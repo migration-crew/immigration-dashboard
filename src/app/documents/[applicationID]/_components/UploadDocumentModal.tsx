97dbcc38f4f094188f08672d485d67b97b05e298
yuki <yukihiguchi0826@gmail.com>
2024-11-24 01:35:09 -0800

    modify SubmitDocumentButton

--- src/app/documents/[applicationID]/_components/UploadDocumentModal.tsx
+++ src/app/documents/[applicationID]/_components/UploadDocumentModal.tsx
@@ -1,44 +1,29 @@
-import { Copy, Plus } from "lucide-react"
- 
-import { Button } from "@/components/ui/upImmigrationButton"
 import {
   Dialog,
   DialogClose,
   DialogContent,
-  DialogDescription,
   DialogFooter,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { SubmitDocumentButton } from "./SubmitDocumentButton"
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/upImmigrationButton";
+import { DocumentDropzone } from "./DocumentDropzone";
+import { SubmitDocumentButton } from "./SubmitDocumentButton";
 
 type Props = {
-  status: string
-}
+  status: string;
+};
 
-export const UploadDocumentModal = ({status}: Props) => {
-
+export const UploadDocumentModal = ({ status }: Props) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        {status === "NotSubmitted" ? (<Button className="px-4 py-[6px] h-[33px] w-[148px] justify-center"><Plus />Add</Button>) : (<SubmitDocumentButton status={status} />)}
+        <SubmitDocumentButton status={status} />
       </DialogTrigger>
       <DialogContent className="sm:max-w-md">
-        <DialogHeader>
-          <DialogTitle>Documents</DialogTitle>
-          <DialogDescription>
-            Please upload document to here.
-          </DialogDescription>
-        </DialogHeader>
-        
+        <DocumentDropzone />
         <DialogFooter className="sm:justify-end">
           <DialogClose asChild>
-            <Button type="button">
-              Submit
-            </Button>
+            <Button type="button">Submit</Button>
           </DialogClose>
         </DialogFooter>
       </DialogContent>
