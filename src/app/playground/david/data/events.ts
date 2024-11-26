90175e58f2a97b94e43eaf7b06cf31d94e72d21e
David Torrez <dtorrez1996@gmail.com>
2024-11-25 19:06:58 -0500

    Create events array in david > data

---
+++ src/app/playground/david/data/events.ts
@@ -0,0 +1,19 @@
+import { QuickCalendarType } from "@/types/Calendar/EventType";
+
+export const events: QuickCalendarType[] = [
+  {
+    id: "1",
+    startDate: "2022-01-01T00:00:00.000Z",
+    duration: 60,
+  },
+  {
+    id: "2",
+    startDate: "2022-01-02T00:00:00.000Z",
+    duration: 60,
+  },
+  {
+    id: "3",
+    startDate: "2022-01-03T00:00:00.000Z",
+    duration: 60,
+  },
+];
