e0e92523ea6af99f0457bdbc5719172d6d0c13f2
YuiKonaka <youthfuldays585@gmail.com>
2024-11-26 12:56:06 -0800

    Created ChatHeader

--- src/app/inbox/_components/ChatHeader.tsx
+++ src/app/inbox/_components/ChatHeader.tsx
@@ -4,7 +4,7 @@ export default function ChatHeader() {
   return (
     <>
       <div className="flex w-[846px] h-[62px] border-b rounded-lg px-4 py-3">
-        <SubheadingLead>#Student</SubheadingLead>
+        <SubheadingLead>#All Student</SubheadingLead>
       </div>
     </>
   );
