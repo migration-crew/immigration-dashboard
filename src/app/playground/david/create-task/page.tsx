88c3e2b1759c3e5be5862ebc1ae1baa096fd9fbf
David Torrez <dtorrez1996@gmail.com>
2024-11-26 15:23:35 -0500

    Import CreateTaskForm component in CreateTaskPage

--- src/app/playground/david/create-task/page.tsx
+++ src/app/playground/david/create-task/page.tsx
@@ -1,9 +1,10 @@
 import { PageContainer } from "@/components/common/PageContainer";
+import { CreateTaskForm } from "../CreateTaskForm";
 
 export default function CreateTaskPage() {
   return (
     <PageContainer>
-      <p>Create task page</p>
+      <CreateTaskForm />
     </PageContainer>
   );
 }
