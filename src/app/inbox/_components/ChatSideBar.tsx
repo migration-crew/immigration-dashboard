215c9ea3762dbd05fedfc3873d4a881e5ffb707b
YuiKonaka <youthfuldays585@gmail.com>
2024-11-26 10:46:19 -0800

    Implemented serchBar

--- src/app/inbox/_components/ChatSideBar.tsx
+++ src/app/inbox/_components/ChatSideBar.tsx
@@ -1,6 +1,8 @@
 import { CaptionSemi } from "@/components/common/text/CaptionSemi";
 import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/upImmigrationButton";
+import { Search } from "lucide-react";
 
 type Props = {
   channels: string[];
@@ -10,10 +12,21 @@ type Props = {
 export default function ChatSideBar({ channels, messages }: Props) {
   return (
     <div className="w-[290px] h-full">
-      <Card className="flex flex-col gap-6 pt-10 pb-6 px-5">
+      <Card className="flex flex-col gap-6 py-10 px-5">
+        <form className="relative w-full max-w-md mx-auto">
+          <Input
+            type="text"
+            placeholder="Search Users"
+            className="pl-10 pr-4 py-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+          />
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+          <button type="submit" className="sr-only">
+            Search
+          </button>
+        </form>
         <div className="flex flex-col gap-6">
           <CaptionSemi>Channel</CaptionSemi>
-          <div className="flex flex-col gap-2 w-[250px]">
+          <div className="flex flex-col gap-2 w-[250px] bg-inherit hover:bg-primary-gray active:bg-secondary-green">
             {channels.map((channel, index) => (
               <Button key={index} variant="green">
                 {channel}
