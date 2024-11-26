4511e03615d059e6c797fbb76f6d03c227b8b6b0
David Torrez <dtorrez1996@gmail.com>
2024-11-26 15:19:55 -0500

    Restore CreateTaskPage

---
+++ src/app/playground/david/create-task/page.tsx
@@ -0,0 +1,9 @@
+import { PageContainer } from "@/components/common/PageContainer";
+
+export default function CreateTaskPage() {
+  return (
+    <PageContainer>
+      <p>Create task page</p>
+    </PageContainer>
+  );
+}
