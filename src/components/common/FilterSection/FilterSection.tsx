61500fca325481ab0e5fd945bab3176064911180
SauloAC <saulo.andrade.costa@gmail.com>
2024-12-02 11:50:26 -0800

    Updating checkmark

--- src/components/common/FilterSection/FilterSection.tsx
+++ src/components/common/FilterSection/FilterSection.tsx
@@ -4,7 +4,6 @@ import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuRadioItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ScrollArea } from "@/components/ui/scroll-area";
@@ -75,20 +74,22 @@ export default function FilterSection({
         <DropdownMenuContent align="start" className="w-[187px]">
           <ScrollArea className="h-[200px] w-full">
             {sortOptions.map((option) => (
-              <DropdownMenuRadioItem
+              <div
                 key={option.value}
-                value={option.value}
-                className="flex items-center justify-between py-2 px-4 cursor-pointer hover:bg-gray-100"
+                className="flex items-center py-2 px-4 gap-2 cursor-pointer hover:bg-gray-100"
                 onClick={() => {
                   setSelectedSort(option.value);
                   onSortChange(option.value);
                 }}
               >
-                <Caption>{option.label}</Caption>
                 {selectedSort === option.value && (
                   <Check className="h-4 w-4 text-bold" />
                 )}
-              </DropdownMenuRadioItem>
+                {selectedSort !== option.value && (
+                  <div className="h-4 w-4"></div>
+                )}
+                <Caption>{option.label}</Caption>
+              </div>
             ))}
           </ScrollArea>
           {selectedSort.length > 0 && (
