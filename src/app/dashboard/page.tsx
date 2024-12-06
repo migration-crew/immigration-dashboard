1e96667693c93fcbf1ad7ba9b617d2010eb237ec
David Torrez <dtorrez1996@gmail.com>
2024-12-06 03:05:39 -0500

    Implement Breadcrumbs and ApplicationSwitcher

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,28 +1,32 @@
-import { Button } from '@/components/ui/upImmigrationButton'
-import Link from 'next/link'
-import React from 'react'
+import ApplicationSwitcher from "@/components/common/ApplicationSwitcher";
+import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
+import { PageContainer } from "@/components/common/PageContainer";
+import { applications } from "@/data/applications";
+import { progresses } from "@/data/progresses";
+import { StageProgressType } from "@/types/Application/ApplicationType";
 
-const page = () => {
-  return (
-    <div>
-      <p>dashbord</p>
-      <Button asChild>
-        <Link href="/payments/1">1</Link>
-      </Button>
-      <Button asChild>
-        <Link href="/payments/2">2</Link>
-      </Button>
-      <Button asChild>
-        <Link href="/payments/3">3</Link>
-      </Button>
-      <Button asChild>
-        <Link href="/payments/4">4</Link>
-      </Button>
-      <Button asChild>
-        <Link href="/payments/5">5</Link>
-      </Button>
-    </div>
-  )
+const INITIAL_LINKS = [
+  { name: "Dashboard", href: "/dashboard" },
+  { name: "Loading...", href: "#" },
+];
+
+async function getProgresses(): Promise<StageProgressType[]> {
+  /* const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const response = await fetch(`${apiUrl}/api/progresses`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch progresses");
+  } */
+  // return await response.json();
+  return progresses;
 }
 
-export default page
+export default async function DashboardPage() {
+  return (
+    <PageContainer>
+      <div className="flex justify-between items-center">
+        <BreadcrumbComponent links={INITIAL_LINKS} />
+        <ApplicationSwitcher applications={applications} />
+      </div>
+    </PageContainer>
+  );
+}
