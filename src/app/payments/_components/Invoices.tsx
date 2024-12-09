99f9aca92110e2e3cd580b697c9ae8af16f04ecd
YuiKonaka <youthfuldays585@gmail.com>
2024-12-09 11:19:18 -0800

    Conpleted Invoices component

--- src/app/payments/_components/Invoices.tsx
+++ src/app/payments/_components/Invoices.tsx
@@ -1,13 +1,15 @@
+"use client";
 import { Caption } from "@/components/common/text/Caption";
 import { Button } from "@/components/ui/upImmigrationButton";
 import {
   Table,
   TableBody,
   TableCell,
-  TableHeader,
   TableRow,
 } from "@/components/ui/upImmigrationTable";
 import { InvoicePaymentType } from "@/types/Payment/PaymentType";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useState } from "react";
 import { LiaDownloadSolid } from "react-icons/lia";
 
 type InvoiceTable = {
@@ -21,31 +23,57 @@ type Props = {
 };
 
 export default function Invoices({ invoiceData }: Props) {
-  console.log("invoiceData:", invoiceData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = invoiceData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(invoiceData.length / itemsPerPage);
+
+  const handlePrevious = () => {
+    setCurrentPage((prev: number) => Math.max(prev - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev: number) => Math.min(prev + 1, totalPages));
+  };
+
   return (
     <>
+      <div className="py-4 border-b">
+        <Caption className=" h-[21px] w-auto">
+          This data is reported once at 0700hrs local time every day
+        </Caption>
+      </div>
       <Table className="w-[1152px]">
-        <TableHeader className="px-[30px] h-[21px] border-b-2">
-          <Caption className="py-4">
-            This data is reported once at 0700hrs local time every day
-          </Caption>
-        </TableHeader>
-        <TableBody className="px-2">
-          {invoiceData.map((invoiceType, index) => (
-            <TableRow key={index} className="h-[49px] px-2">
-              <TableCell className="text-caption-semi w-[20%] ">
+        <TableBody className="px-2  border-b">
+          {currentItems.map((invoiceType, index) => (
+            <TableRow key={index} className="h-[49px]">
+              <TableCell className="text-caption-semi w-[17%] ">
                 {invoiceType.invoice.id}
               </TableCell>
-              <TableCell className="text-caption-semi w-[20%]">
+              <TableCell className="text-caption-semi w-[36%]">
                 {invoiceType.name}
               </TableCell>
-              <TableCell className="text-caption-semi w-[20%]">
+              <TableCell className="text-caption-semi w-[17%]">
                 {invoiceType.type}
               </TableCell>
-              <TableCell className="text-caption-semi w-[20%]">
+              <TableCell
+                className={`text-caption-semi w-[17%] ${
+                  invoiceType.invoice.status === "Paid"
+                    ? "text-secondary-green"
+                    : invoiceType.invoice.status === "Refunded"
+                    ? "text-secondary-medium-gray"
+                    : invoiceType.invoice.status === "Unpaid"
+                    ? "text-primary-red"
+                    : ""
+                }`}
+              >
                 {invoiceType.invoice.status}
               </TableCell>
-              <TableCell className="w-[20%] h-[33px] ">
+              <TableCell className="w-[13px] h-[33px] ">
                 <Button className="text-primary-red bg-primary-white text-caption-semi">
                   Download
                   <LiaDownloadSolid className="text-primary-red" />
@@ -55,6 +83,29 @@ export default function Invoices({ invoiceData }: Props) {
           ))}
         </TableBody>
       </Table>
+      <div className="flex items-center py-2">
+        <div className="flex w-auto h-auto space-x-0">
+          <Button
+            onClick={handlePrevious}
+            disabled={currentPage === 1}
+            className="bg-primary-white text-caption-semi w-[43px] h-[30px] rounded-none rounded-l-lg shadow-none border"
+          >
+            <ChevronLeft className="" />
+          </Button>
+          <Button
+            onClick={handleNext}
+            disabled={currentPage === totalPages}
+            className="bg-primary-white text-caption-semi  w-[43px] h-[30px] rounded-none rounded-r-lg shadow-none border-y border-r"
+          >
+            <ChevronRight className="" />
+          </Button>
+        </div>
+        {/* <div className="text-muted-foreground text-caption-semi items-center"> */}
+        <div className="text-caption-semi text-muted-foreground item-center ml-2">
+          Showing {currentPage} of {totalPages}
+        </div>
+        {/* </div> */}
+      </div>
     </>
   );
 }
--- src/app/playground/yui/page.tsx
+++ src/app/playground/yui/page.tsx
@@ -277,6 +277,141 @@ const page = () => {
             {
               invoice: {
                 id: "INV-00104",
+                status: "Unpaid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_ESL",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "nRHegHAxQG",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI_2",
+              type: "Admission",
+            },
+            {
+              invoice: {
+                id: "INV-00202",
+                status: "Refunded",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "INV-00104",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_ESL",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "nRHegHAxQG",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI_2",
+              type: "Admission",
+            },
+            {
+              invoice: {
+                id: "INV-00202",
+                status: "Refunded",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "INV-00104",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_ESL",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "nRHegHAxQG",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI_2",
+              type: "Admission",
+            },
+            {
+              invoice: {
+                id: "INV-00202",
+                status: "Refunded",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "INV-00104",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_ESL",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "nRHegHAxQG",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI_2",
+              type: "Admission",
+            },
+            {
+              invoice: {
+                id: "INV-00202",
+                status: "Refunded",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "INV-00104",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_ESL",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "nRHegHAxQG",
+                status: "Paid",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI_2",
+              type: "Admission",
+            },
+            {
+              invoice: {
+                id: "INV-00202",
+                status: "Refunded",
+                invoiceURL: "",
+              },
+              name: "Maria_CICCC_UX/UI",
+              type: "Tuition",
+            },
+            {
+              invoice: {
+                id: "INV-00104",
                 status: "Paid",
                 invoiceURL: "",
               },
