95e2f82ca67082036b473396cde690b84ac25751
yuki <yukihiguchi0826@gmail.com>
2024-12-11 20:29:09 -0800

    change filter section with query parameter

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,33 +1,30 @@
-import ApplicationSwitcher from '@/components/common/ApplicationSwitcher';
-import { BreadcrumbComponent } from '@/components/common/Breadcrumbs/BreadcrumbComponent'
-import FilterSection from '@/components/common/FilterSection/FilterSection';
-import { PageContainer } from '@/components/common/PageContainer'
-import React from 'react'
-import { DocumentTable } from './_components/DocumentTable';
-import { documents } from '../playground/yuki/data/DocumentTable';
-import { NewDocForm } from './_components/NewDocForm';
-import { applications } from '../playground/saulo/data/application';
+import ApplicationSwitcher from "@/components/common/ApplicationSwitcher";
+import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
+import { PageContainer } from "@/components/common/PageContainer";
+import { applications } from "../playground/saulo/data/application";
+import { FilterTable } from "./_components/FilterTable";
+import { NewDocForm } from "./_components/NewDocForm";
 
 const documentPage = () => {
-  const isAdmin = true
+  const isAdmin = true;
   const links = [
-    { name: "DocumentManagement", href: "/documents" },
-    { name: "mariaciccc", href: "/documents" },
+    { name: "Documents", href: "/documents" },
+    { name: "Maria-CICCC", href: "/documents" },
   ];
+
   return (
-    <PageContainer className='flex flex-col pt-1'>
-      <div className='flex justify-between items-end'>
+    <PageContainer className="flex flex-col">
+      <div className="flex justify-between items-center">
         <BreadcrumbComponent links={links} />
         <ApplicationSwitcher applications={applications} />
       </div>
-      {/* <FilterSection  /> */}
-      <div className='flex-1 flex flex-col justify-between gap-6'>
-      <DocumentTable documents={documents} />
-      {isAdmin && <NewDocForm />}
+      <div className="flex-1 flex flex-col justify-between gap-6">
+        
+        <FilterTable />
+        {isAdmin && <NewDocForm />}
       </div>
-      
     </PageContainer>
-  )
-}
+  );
+};
 
-export default documentPage
+export default documentPage;
