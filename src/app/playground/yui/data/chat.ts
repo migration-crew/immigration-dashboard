32ebf503a875cbb7d962370dd2689be17e5224ef
YuiKonaka <youthfuldays585@gmail.com>
2024-12-11 16:55:29 -0800

    Completed inbox Page

---
+++ src/app/playground/yui/data/chat.ts
@@ -0,0 +1,117 @@
+export const chatsData = [
+  {
+    id: "0",
+    name: "Saulo",
+    members: [
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+    ],
+  },
+  {
+    id: "0",
+    name: "Saulo2",
+    members: [
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+    ],
+  },
+  {
+    id: "0",
+    name: "Saulo3",
+    members: [
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+    ],
+  },
+  {
+    id: "0",
+    name: "Saulo4",
+    members: [
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+    ],
+  },
+  {
+    id: "0",
+    name: "Saulo5",
+    members: [
+      {
+        id: "0",
+        firstName: "Yuki",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+      {
+        id: "1",
+        firstName: "Zoro",
+        imageUrl: "https://github.com/shadcn.png",
+      },
+    ],
+  },
+];
---
+++ src/app/playground/yui/data/message.ts
@@ -0,0 +1,44 @@
+export const messages = [
+  {
+    id: "0",
+    content: "Hello",
+    createdAt: "5:20",
+    user: {
+      id: "userId",
+      imageUrl: "https://github.com/shadcn.png",
+      firstName: "Broock",
+    },
+  },
+  {
+    id: "1",
+    content: "Good bye",
+    createdAt: "9:20",
+    user: {
+      id: "adminId",
+      imageUrl: "https://github.com/shadcn.png",
+      firstName: "Fruncky",
+    },
+  },
+  {
+    id: "0",
+    content:
+      "oihosjdnvosdenvoisdenvoksndovnsodvnpsdknvopsiednvpoksndvolisndeboknselbjzn;dijrbodsjnbf;vojnsd;ofivn;oeksvn;osenv;ojnds;ojvn;dfojnb;ojdnf;bjovn;sdojnb;ovjsdn;vk;sodeinv;oinegw;rong;oiwegoinwe;okgnwoi;esgvibewsiugblvisedblviubeilgubewibgvijbdsjbgvlierbivueiuvnlisudnlivndlsinvlisndlivnlisdnvidsnbvilndsibn",
+    createdAt: "10:20",
+    user: {
+      id: "userId",
+      imageUrl: "https://github.com/shadcn.png",
+      firstName: "Broock",
+    },
+  },
+  {
+    id: "1",
+    content:
+      "ijokkkkkkkkkkkkkkookkokookasfkofokfodsjndsjidsjdjddjnsdjndndnsdnsvvdsnjnjsijdvjnsdvndvjndjnvndvjsndvdvanvnajdovdavdamvadamvahuuovdaodavdvahudvavadmuhdavmhdvavmavdaouhvmdhamvadouvdaovhadmovdhouvdauhovdahuvadhuvdhuavdahuvadouvdaohudvahuvodauhovuadhvadouhhoudvahdovavadhoovhdahvduaodvahuadovhovduahvdauhdvaoavdohvdaohumvhdaomvhdoavodhuahoavdudhovuvadhouvdahouadvhuovadhouavdohuadvhouavdhouvadhouvadhouvdahouavdhouvadhouvdaohvdahmovadohuvadohuvadohuvadhovadhouvadohuvadhuovadhouadvhohudvahouvdaohvadavdhoadvhouadvhuoadvohuavddva",
+    createdAt: "11:20",
+    user: {
+      id: "adminId",
+      imageUrl: "https://github.com/shadcn.png",
+      firstName: "Fruncky",
+    },
+  },
+];
