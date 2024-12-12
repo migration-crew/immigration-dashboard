f33f1664a3466a258b233ab974c870630fc449bf
kamimi <awakaru0122@gmail.com>
2024-12-12 09:42:47 -0800

    :sparkles: implement protected route by clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import TopNavbar from "@/components/common/TopNavbar/TopNavbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Poppins } from "next/font/google";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import ProtectedRoute from "./protectedRoute/ProtectedRoute";
 
 export const poppins = Poppins({
   subsets: ["latin"],
@@ -28,14 +29,23 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${poppins.className} antialiased min-h-screen flex`}>
-          {modal}
-          <SidebarProvider>
-            <AppSidebar />
-            <main className="flex flex-col flex-1">
-              <TopNavbar />
+          <ProtectedRoute>
+            {/* Signed In */}
+            <SignedIn>
+              {modal}
+              <SidebarProvider>
+                <AppSidebar />
+                <main className="flex flex-col flex-1">
+                  <TopNavbar />
+                  {children}
+                </main>
+              </SidebarProvider>
+            </SignedIn>
+            {/* Signed Out */}
+            <SignedOut>
               {children}
-            </main>
-          </SidebarProvider>
+            </SignedOut>
+          </ProtectedRoute>
         </body>
       </html>
     </ClerkProvider>
--- src/app/signin/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import * as Clerk from '@clerk/elements/common'
-import * as SignIn from '@clerk/elements/sign-in'
-
-export default function SignInPage() {
-  return <SignIn.Root>[Sign In Root]</SignIn.Root>
-}
