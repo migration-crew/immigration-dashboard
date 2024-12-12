057a8abe0434aa7e7e5bd9ffd4a1b7f24f233e77
yuki <yukihiguchi0826@gmail.com>
2024-12-11 20:55:28 -0800

    implement calendar page and appointment page

---
+++ src/app/appointment/page.tsx
@@ -0,0 +1,13 @@
+import { PageContainer } from '@/components/common/PageContainer'
+import React from 'react'
+import { AppointmentCard } from './_components/AppoitmentCard'
+
+const page = () => {
+  return (
+    <PageContainer>
+      {/* <AppointmentCard appointment={} attendees={} /> */}
+    </PageContainer>
+  )
+}
+
+export default page
