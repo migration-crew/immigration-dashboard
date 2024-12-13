83f199985a2c8c3a3aa5b8b324910ae18cdc9234
kamimi <awakaru0122@gmail.com>
2024-12-12 18:39:13 -0800

    :bug: fix typo

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/upImmigrationButton'
 
 const page = () => {
   const links = [
-    { name: "Calender", href: "/calender" },
+    { name: "Calendar", href: "/calendar" },
     { name: "Appointment", href: "/appointment" },
   ];
   return (
