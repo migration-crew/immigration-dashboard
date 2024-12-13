234e922a2a099c8b40f64cc46f5f1e9f37d10511
yuki <yukihiguchi0826@gmail.com>
2024-12-12 20:26:38 -0800

    change application switcher data and position

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,7 +1,7 @@
 import ApplicationSwitcher from "@/components/common/ApplicationSwitcher";
 import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
 import { PageContainer } from "@/components/common/PageContainer";
-import { applications } from "../playground/saulo/data/application";
+import { applications } from "@/data/applications";
 import { FilterTable } from "./_components/FilterTable";
 import { NewDocForm } from "./_components/NewDocForm";
 
@@ -19,7 +19,6 @@ const documentPage = () => {
         <ApplicationSwitcher applications={applications} />
       </div>
       <div className="flex-1 flex flex-col justify-between gap-6">
-        
         <FilterTable />
         {isAdmin && <NewDocForm />}
       </div>
