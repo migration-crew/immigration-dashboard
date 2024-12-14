037a17c7354f34d654f3e560b26b79cd870842b4
kamimi <awakaru0122@gmail.com>
2024-12-13 16:59:11 -0800

    :bug: change build-in clerk components to custom ui

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,45 +4,74 @@ import * as React from 'react'
 import { useSignIn } from '@clerk/nextjs'
 import { useRouter } from 'next/navigation'
 import { BannerImage } from '../_components/BannerImage'
-import { SignIn } from '@clerk/nextjs'
-import { LogoImage } from '../../../components/common/Logo'
+import { LogoImage } from '@/components/common/Logo'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/upImmigrationButton"
 
 export default function Page() {
-  // FIXME: use the following code later to customize the sign-in page
-  // const { isLoaded, signIn, setActive } = useSignIn()
-  // const [email, setEmail] = React.useState('')
-  // const [password, setPassword] = React.useState('')
-  // const router = useRouter()
+  const { isLoaded, signIn, setActive } = useSignIn()
+  const router = useRouter()
 
-  // // Handle the submission of the sign-in form
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault()
+  const formSchema = z.object({
+    email: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    password: z.string().min(2, {
+      message: "Password must be at least 2 characters.",
+    }),
+  })
 
-  //   if (!isLoaded) return
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  })
 
-  //   // Start the sign-in process using the email and password provided
-  //   try {
-  //     const signInAttempt = await signIn.create({
-  //       identifier: email,
-  //       password,
-  //     })
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
 
-  //     // If sign-in process is complete, set the created session as active
-  //     // and redirect the user
-  //     if (signInAttempt.status === 'complete') {
-  //       await setActive({ session: signInAttempt.createdSessionId })
-  //       router.push('/')
-  //     } else {
-  //       // If the status is not complete, check why. User may need to
-  //       // complete further steps.
-  //       console.error(JSON.stringify(signInAttempt, null, 2))
-  //     }
-  //   } catch (err: any) {
-  //     // See https://clerk.com/docs/custom-flows/error-handling
-  //     // for more info on error handling
-  //     console.error(JSON.stringify(err, null, 2))
-  //   }
-  // }
+    if (!isLoaded) return
+
+    // Start the sign-in process using the email and password provided
+    try {
+      const signInAttempt = await signIn.create({
+        identifier: values.email,
+        password: values.password,
+      })
+
+      // If sign-in process is complete, set the created session as active
+      // and redirect the user
+      if (signInAttempt.status === 'complete') {
+        await setActive({ session: signInAttempt.createdSessionId })
+        router.push('/')
+      } else {
+        // If the status is not complete, check why. User may need to
+        // complete further steps.
+        console.error(JSON.stringify(signInAttempt, null, 2))
+      }
+    } catch (err: any) {
+      // See https://clerk.com/docs/custom-flows/error-handling
+      // for more info on error handling
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
 
   // Display a form to capture the user's email and password
   return (
@@ -53,8 +82,39 @@ export default function Page() {
         </div>
 
         <div className="w-1/2 h-full flex flex-col gap-16 items-center justify-center bg-white">
-          <LogoImage width={215} height={102}/>
-          <SignIn />
+          <LogoImage width={215} height={102} />
+
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email address:</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password:</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Sign In</Button>
+            </form>
+          </Form>
         </div>
       </div>
     </>
