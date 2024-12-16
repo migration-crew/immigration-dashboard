ab420d074e9d3aa16f786ceb17c3e6ca66c60f57
yuki <yukihiguchi0826@gmail.com>
2024-12-16 10:17:39 -0800

    make application schema pritier

--- src/app/api/schemas/application/applicationType.schema.ts
+++ src/app/api/schemas/application/applicationType.schema.ts
@@ -1,21 +1,33 @@
-import { Model, Schema, model, models } from "mongoose";
+import { Model, Schema, model } from "mongoose";
 import { ApplicationTypeType } from "../../types/applicationType";
 
+type ApplicationTypeModelType = Model<ApplicationTypeType>;
 
-type ApplicationTypeModelType = Model<ApplicationTypeType>
-
-const ApplicationTypeSchema = new Schema<ApplicationTypeType, ApplicationTypeModelType>(
+const ApplicationTypeSchema = new Schema<
+  ApplicationTypeType,
+  ApplicationTypeModelType
+>(
   {
     name: { type: String, required: true },
-    applicationStages: [{ 
-      name: { type: String, required: true },
-      applicationTasks: { type: [Schema.Types.ObjectId], ref: "ApplicationTask", required: true }
-    }]
+    applicationStages: [
+      {
+        name: { type: String, required: true },
+        applicationTasks: {
+          type: [Schema.Types.ObjectId],
+          ref: "ApplicationTask",
+          required: true,
+        },
+      },
+    ],
   },
   {
-    timestamps: true, versionKey: false
+    timestamps: true,
+    versionKey: false,
   }
 );
 
-const ApplicationType = model<ApplicationTypeType, ApplicationTypeModelType>("ApplicationType", ApplicationTypeSchema);
+const ApplicationType = model<ApplicationTypeType, ApplicationTypeModelType>(
+  "ApplicationType",
+  ApplicationTypeSchema
+);
 export default ApplicationType;
