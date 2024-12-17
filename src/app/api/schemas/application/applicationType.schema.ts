3531bbfa63efdad8ff2c78d8dc47cc3ea040da43
David Torrez <dtorrez1996@gmail.com>
2024-12-17 13:36:31 -0500

    Modify applicationTypeSchema

--- src/app/api/schemas/application/applicationType.schema.ts
+++ src/app/api/schemas/application/applicationType.schema.ts
@@ -1,21 +1,36 @@
 import { Model, Schema, model, models } from "mongoose";
 import { ApplicationTypeType } from "../../types/applicationType";
 
+type ApplicationTypeModelType = Model<ApplicationTypeType>;
 
-type ApplicationTypeModelType = Model<ApplicationTypeType>
-
-const ApplicationTypeSchema = new Schema<ApplicationTypeType, ApplicationTypeModelType>(
+const ApplicationTypeSchema = new Schema<
+  ApplicationTypeType,
+  ApplicationTypeModelType
+>(
   {
     name: { type: String, required: true },
-    applicationStages: [{ 
-      name: { type: String, required: true },
-      applicationTasks: { type: [Schema.Types.ObjectId], ref: "ApplicationTask", required: true }
-    }]
+    applicationStages: [
+      {
+        name: { type: String, required: true },
+        applicationTasks: {
+          type: [Schema.Types.ObjectId],
+          ref: "ApplicationTask",
+          required: true,
+        },
+      },
+    ],
   },
   {
-    timestamps: true, versionKey: false
+    timestamps: true,
+    versionKey: false,
   }
 );
 
-const ApplicationType = model<ApplicationTypeType, ApplicationTypeModelType>("ApplicationType", ApplicationTypeSchema);
+//const ApplicationType = model<ApplicationTypeType, ApplicationTypeModelType>("ApplicationType", ApplicationTypeSchema);
+const ApplicationType =
+  models.ApplicationType ||
+  model<ApplicationTypeType, ApplicationTypeModelType>(
+    "ApplicationType",
+    ApplicationTypeSchema
+  );
 export default ApplicationType;
