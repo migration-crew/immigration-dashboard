92b3e1879c338f0ae85c69cf7c60bbc90fa26bc0
kamimi <awakaru0122@gmail.com>
2024-12-19 14:34:27 -0800

    :bug: fix inbox texts

--- src/app/playground/yui/data/message.ts
+++ src/app/playground/yui/data/message.ts
@@ -1,7 +1,7 @@
 export const messages = [
   {
     id: "0",
-    content: "Hello",
+    content: "Welcome!",
     createdAt: "5:20",
     user: {
       id: "userId",
@@ -11,7 +11,7 @@ export const messages = [
   },
   {
     id: "1",
-    content: "Good bye",
+    content: "Hi",
     createdAt: "9:20",
     user: {
       id: "adminId",
@@ -21,8 +21,7 @@ export const messages = [
   },
   {
     id: "3",
-    content:
-      "oihosjdnvosdenvoisdenvoksndovnsodvnpsdknvopsiednvpoksndvolisndeboknselbjzn;dijrbodsjnbf;vojnsd;ofivn;oeksvn;osenv;ojnds;ojvn;dfojnb;ojdnf;bjovn;sdojnb;ovjsdn;vk;sodeinv;oinegw;rong;oiwegoinwe;okgnwoi;esgvibewsiugblvisedblviubeilgubewibgvijbdsjbgvlierbivueiuvnlisudnlivndlsinvlisndlivnlisdnvidsnbvilndsibn",
+    content: "📢 Exciting News for Canada Visa Applicants! 📢\nHi everyone,\nWe have some exciting news to share! Canada has introduced new visa policies that significantly streamline the application process. This means less paperwork and faster approvals, making it easier than ever to get your visa. We invite you to join our online info session on July 31, where we'll go over these changes in detail and answer any questions you might have. Please RSVP by October 06 to secure your spot. In light of these positive changes, we are also offering a 15% discount on our services for a limited time. Make sure to check your inbox for more details on how to take advantage of this offer!",
     createdAt: "10:20",
     user: {
       id: "userId",
@@ -32,8 +31,7 @@ export const messages = [
   },
   {
     id: "2",
-    content:
-      "ijokkkkkkkkkkkkkkookkokookasfkofokfodsjndsjidsjdjddjnsdjndndnsdnsvvdsnjnjsijdvjnsdvndvjndjnvndvjsndvdvanvnajdovdavdamvadamvahuuovdaodavdvahudvavadmuhdavmhdvavmavdaouhvmdhamvadouvdaovhadmovdhouvdauhovdahuvadhuvdhuavdahuvadouvdaohudvahuvodauhovuadhvadouhhoudvahdovavadhoovhdahvduaodvahuadovhovduahvdauhdvaoavdohvdaohumvhdaomvhdoavodhuahoavdudhovuvadhouvdahouadvhuovadhouavdohuadvhouavdhouvadhouvadhouvdahouavdhouvadhouvdaohvdahmovadohuvadohuvadohuvadhovadhouvadohuvadhuovadhouadvhohudvahouvdaohvadavdhoadvhouadvhuoadvohuavddva",
+    content: "Hi! I'm interested in finding a 2-bedroom apartment in Toronto. My budget is around $2,000 per month, and I’d like it to be near public transportation and amenities like grocery stores and parks. Can you help me with this?",
     createdAt: "11:20",
     user: {
       id: "adminId",
