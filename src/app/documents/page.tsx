8f91a4cc2cc088e2dbeb91938a82f27046bdc9a9
David Torrez <dtorrez1996@gmail.com>
2025-01-30 12:11:27 -0500

    Update breadcrumb name in Documents page

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,10 +1,10 @@
-import ApplicationSwitcher from "@/components/common/ApplicationSwitcher";
-import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
-import { PageContainer } from "@/components/common/PageContainer";
+import ApplicationSwitcher from '@/components/common/ApplicationSwitcher';
+import { BreadcrumbComponent } from '@/components/common/Breadcrumbs/BreadcrumbComponent';
+import { PageContainer } from '@/components/common/PageContainer';
 // import { applications } from "@/data/applications";
-import { getAllApplications } from "@/hooks/getAllApplications";
-import { FilterTable } from "./_components/FilterTable";
-import { NewDocForm } from "./_components/NewDocForm";
+import { getAllApplications } from '@/hooks/getAllApplications';
+import { FilterTable } from './_components/FilterTable';
+import { NewDocForm } from './_components/NewDocForm';
 
 const documentPage = async ({
   searchParams,
@@ -15,19 +15,19 @@ const documentPage = async ({
   const applications = await getAllApplications();
 
   const applicationId = searchParams.applicationId || applications[0]._id;
-  
+
   const links = [
-    { name: "Documents", href: "/documents" },
-    { name: "Maria_CICCC_UX/UI_2", href: "/documents" },
+    { name: 'Documents', href: '/documents' },
+    { name: 'João_CICCC_ESL', href: '/documents' },
   ];
 
   return (
-    <PageContainer className="flex flex-col">
-      <div className="flex justify-between items-center">
+    <PageContainer className='flex flex-col'>
+      <div className='flex justify-between items-center'>
         <BreadcrumbComponent links={links} />
         <ApplicationSwitcher applications={applications} />
       </div>
-      <div className="flex-1 flex flex-col justify-between gap-6">
+      <div className='flex-1 flex flex-col justify-between gap-6'>
         <FilterTable applicationId={applicationId} />
         {isAdmin && <NewDocForm />}
       </div>
