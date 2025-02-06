ee50b044a798b64b35f431cbe2454e4326d9b915
David Torrez <dtorrez1996@gmail.com>
2025-02-06 01:58:22 -0500

    Add SheetTitle to solve error on TaskDetail component

--- src/app/playground/david/TaskDetail.tsx
+++ src/app/playground/david/TaskDetail.tsx
@@ -1,8 +1,8 @@
-import { Microtext } from "@/components/common/text/Microtext";
-import { MicrotextSemi } from "@/components/common/text/MicrotextSemi";
-import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import { SheetContent } from "@/components/ui/upImmigrationSheet";
-import { ApplicationTaskType } from "@/types/Application/ApplicationTaskType";
+import { Microtext } from '@/components/common/text/Microtext';
+import { MicrotextSemi } from '@/components/common/text/MicrotextSemi';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { SheetContent, SheetTitle } from '@/components/ui/upImmigrationSheet';
+import { ApplicationTaskType } from '@/types/Application/ApplicationTaskType';
 
 type Props = {
   applicationTask: ApplicationTaskType;
@@ -11,50 +11,52 @@ type Props = {
 export default function TaskDetail({ applicationTask }: Props) {
   return (
     <SheetContent
-      side={"right"}
-      className="flex flex-col w-[320px] bg-primary-white gap-4"
+      side={'right'}
+      className='flex flex-col w-[320px] bg-primary-white gap-4'
     >
-      <MicrotextSemi>{applicationTask.name}</MicrotextSemi>
-      <div className="flex flex-col gap-1">
+      <SheetTitle>
+        <MicrotextSemi>{applicationTask.name}</MicrotextSemi>
+      </SheetTitle>
+      <div className='flex flex-col gap-1'>
         <MicrotextSemi>Due Date:</MicrotextSemi>
         <Microtext>{applicationTask.dueDate}</Microtext>
       </div>
-      <div className="flex flex-col gap-1">
+      <div className='flex flex-col gap-1'>
         <MicrotextSemi>Task Description:</MicrotextSemi>
         <Microtext>{applicationTask.description}</Microtext>
       </div>
-      <div className="flex flex-col gap-1">
+      <div className='flex flex-col gap-1'>
         <MicrotextSemi>Attachments:</MicrotextSemi>
         <Microtext>{applicationTask.documentURLs}</Microtext>
       </div>
-      <div className="flex flex-col gap-1">
+      <div className='flex flex-col gap-1'>
         <MicrotextSemi>Messages:</MicrotextSemi>
-        <ul className="flex flex-col gap-2">
+        <ul className='flex flex-col gap-2'>
           {applicationTask.comments?.map((comment, index) => (
-            <li key={index} className="flex items-start gap-2">
+            <li key={index} className='flex items-start gap-2'>
               <Avatar>
                 <AvatarImage
                   // src={comment.sender?.imageURL}
-                  src="/images/OliveiraAnderson.jpg"
+                  src='/images/OliveiraAnderson.jpg'
                   alt={`${comment.sender?.firstName}`}
                 />
               </Avatar>
-              <div className="flex flex-col">
-                <div className="flex items-center gap-2">
+              <div className='flex flex-col'>
+                <div className='flex items-center gap-2'>
                   <MicrotextSemi>
                     {comment.sender?.firstName} {comment.sender?.lastName}
                   </MicrotextSemi>
-                  <Microtext className="text-secondary-medium-gray">
+                  <Microtext className='text-secondary-medium-gray'>
                     {new Date(comment.createdAt)
                       .toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
+                        hour: '2-digit',
+                        minute: '2-digit',
                       })
 
                       .toLocaleUpperCase()}
                   </Microtext>
                 </div>
-                <Microtext className="text-secondary-medium-gray">
+                <Microtext className='text-secondary-medium-gray'>
                   {comment.content}
                 </Microtext>
               </div>
