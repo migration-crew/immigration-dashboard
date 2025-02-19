93f8a0ff1cf63e02d2003a3b61ab25e9a64f3ef8
David Torrez <dtorrez1996@gmail.com>
2025-02-19 02:04:13 -0500

    Create YourSchedule as a component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,19 +2,13 @@ import ApplicationProgress from '@/components/common/ApplicationProgress/Applica
 import ApplicationSwitcher from '@/components/common/ApplicationSwitcher';
 import AwaitingPayment from '@/components/common/AwaitingPayment/AwaitingPayment';
 import { BreadcrumbComponent } from '@/components/common/Breadcrumbs/BreadcrumbComponent';
-import DynamicHeaderContainer from '@/components/common/DynamicHeaderContainer';
 import { PageContainer } from '@/components/common/PageContainer';
-import { CaptionSemi } from '@/components/common/text/CaptionSemi';
-import { Paragraph } from '@/components/common/text/Paragraph';
-import { Button } from '@/components/ui/upImmigrationButton';
 import { getAllApplications } from '@/hooks/getAllApplications';
 import { getAllPayments } from '@/hooks/getAllPayments';
 import { fetchApplicationTasks } from '@/hooks/getApplicationTasks';
 import { StageProgressType } from '@/types/Application/ApplicationType';
-import { events } from '../playground/david/data/events';
 import { CurrentTasks } from './_components/CurrentTasks';
 import { LatestUpdates } from './_components/LatestUpdates';
-import QuickCalendar from './_components/QuickCalendar';
 
 const INITIAL_LINKS = [
   { name: 'Dashboard', href: '/dashboard' },
@@ -65,17 +59,6 @@ export default async function DashboardPage() {
       <div className='flex pt-2 gap-4 h-[490px]'>
         <CurrentTasks />
         <LatestUpdates />
-        <div className='flex flex-col gap-2 w-[340px]'>
-          <Paragraph>Your schedule</Paragraph>
-          <div className='flex flex-col w-full gap-2'>
-            <DynamicHeaderContainer
-              headerChildren={<CaptionSemi>Mon Dec 16</CaptionSemi>}
-              contentChildren={<QuickCalendar events={events} />}
-              className='w-full '
-            ></DynamicHeaderContainer>
-            <Button className='bg-secondary-green'>See Your Calendar</Button>
-          </div>
-        </div>
       </div>
     </PageContainer>
   );
