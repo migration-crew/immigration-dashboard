66c53f1e87baa0b2ec67a7284ea6c58a45d71bf2
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-02-19 20:44:19 -0800

    adopt david change with applicationType api

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,19 +2,13 @@ import ApplicationProgress from "@/components/common/ApplicationProgress/Applica
 import ApplicationSwitcher from "@/components/common/ApplicationSwitcher";
 import AwaitingPayment from "@/components/common/AwaitingPayment/AwaitingPayment";
 import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
-import DynamicHeaderContainer from "@/components/common/DynamicHeaderContainer";
-import HorizontalProgressBar from "@/components/common/HorizontalProgressBar";
 import { PageContainer } from "@/components/common/PageContainer";
-import { TaskCard } from "@/components/common/TaskCard/TaskCard";
-import { CaptionSemi } from "@/components/common/text/CaptionSemi";
-import { Paragraph } from "@/components/common/text/Paragraph";
-import { Button } from "@/components/ui/upImmigrationButton";
 import { getAllApplications } from "@/hooks/getAllApplications";
 import { getAllPayments } from "@/hooks/getAllPayments";
 import { fetchApplicationTasks } from "@/hooks/getApplicationTasks";
-import { ApplicationTaskType } from "@/types/Application/ApplicationTaskType";
-import QuickCalendar from "../playground/david/QuickCalendar";
-import { events } from "../playground/david/data/events";
+import { CurrentTasks } from "./_components/CurrentTasks";
+import { LatestUpdates } from "./_components/LatestUpdates";
+import { YourSchedule } from "./_components/YourSchedule";
 
 const INITIAL_LINKS = [
   { name: "Dashboard", href: "/dashboard" },
@@ -48,50 +42,9 @@ export default async function DashboardPage() {
         <AwaitingPayment payments={payments} singleCard={true} />
       </div>
       <div className="flex pt-2 gap-4 h-[490px]">
-        <div className="flex flex-col gap-2 w-[390px]">
-          <Paragraph>Current Tasks</Paragraph>
-          <div className="flex flex-col w-full gap-2">
-            <DynamicHeaderContainer
-              headerChildren={<CaptionSemi>{currentTaskStep.name}</CaptionSemi>}
-              contentChildren={
-                <>
-                  <HorizontalProgressBar progress={currentTaskStep.progress} />
-                  <div className="overflow-auto h-[275px] rounded-b-2xl hide-scrollbar">
-                    {currentTaskStep.tasks.map(
-                      (task: ApplicationTaskType, index: number) => {
-                        const lastTask =
-                          index === currentTaskStep.tasks.length - 1;
-
-                        return (
-                          <TaskCard
-                            key={task._id}
-                            applicationTask={task}
-                            className={`w-full p-4 gap-1  ${
-                              lastTask ? "rounded-b-2xl" : ""
-                            }`}
-                          />
-                        );
-                      }
-                    )}
-                  </div>
-                </>
-              }
-              className="w-full"
-            ></DynamicHeaderContainer>
-            <Button className="bg-secondary-green">See Your Tasks</Button>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 w-[340px]">
-          <Paragraph>Your schedule</Paragraph>
-          <div className="flex flex-col w-full gap-2">
-            <DynamicHeaderContainer
-              headerChildren={<CaptionSemi>Mon Dec 16</CaptionSemi>}
-              contentChildren={<QuickCalendar events={events} />}
-              className="w-full "
-            ></DynamicHeaderContainer>
-            <Button className="bg-secondary-green">See Your Calendar</Button>
-          </div>
-        </div>
+        <CurrentTasks currentTaskStep={currentTaskStep} />
+        <LatestUpdates />
+        <YourSchedule />
       </div>
     </PageContainer>
   );
