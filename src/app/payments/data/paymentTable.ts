ed7803d4fa61bf18e55ad7861513fde0a2c515f6
Joao Sinhasique <sinhasique12@gmail.com>
2025-02-19 23:54:46 -0800

    saving before merging main

---
+++ src/app/payments/data/paymentTable.ts
@@ -0,0 +1,93 @@
+import { ApplicationType } from "@/types/Application/ApplicationType";
+import { TablePaymentType } from "@/types/Payment/PaymentType";
+
+export type testPaymentType = TablePaymentType & {
+  application: ApplicationType;
+  dueDate: Date;
+  type: string;
+};
+
+export const testApplication: ApplicationType = {
+  id: "nRHegHAxQG",
+  name: "Maria_CICCC_UX/UI_2",
+  type: {
+    id: "PvpKKAtezj",
+    name: "student",
+    createdAt: new Date("2024-12-17T02:45:17-08:00"),
+    updatedAt: new Date("2024-12-17T02:45:17-08:00"),
+    
+  },
+  createdAt: new Date("2024-12-17T02:45:17-08:00"),
+  updatedAt: new Date("2024-12-17T02:45:17-08:00"),
+};
+
+export const testPayment1: TablePaymentType = {
+  id: "PvpKKAtezj",
+  status: "paid",
+};
+export const testPayment2: TablePaymentType = {
+  id: "UIRclGQIfO",
+  status: "refunded",
+};
+export const testPayment3: TablePaymentType = {
+  id: "sHyvFRqCeu",
+  status: "unPaid",
+};
+export const testPayment4: TablePaymentType = {
+  id: "vlgKNjRZHz",
+  status: "rejected",
+};
+export const testPayment5: TablePaymentType = {
+  id: "yFwPtUXOFT",
+  status: "paid",
+};
+
+export const addPayment1 = {
+  dueDate: new Date("2024-12-17T02:45:17-08:00"),
+  type: "schoolEnrolment",
+};
+
+export const addPayment2 = {
+  dueDate: new Date("2024-02-08T13:34:32-08:00"),
+  type: "visa",
+};
+export const addPayment3 = {
+  dueDate: new Date("2024-10-31T14:10:21-07:00"),
+  type: "schoolTuition",
+};
+export const addPayment4 = {
+  dueDate: new Date("2024-06-06T02:18:33-07:00"),
+  type: "visa",
+};
+export const addPayment5 = {
+  dueDate: new Date("2024-08-31T14:23:41-07:00"),
+  type: "visa",
+};
+
+export const paymentTableData: testPaymentType[] = [
+  {
+    ...testPayment1,
+    ...addPayment1,
+    application: testApplication,
+  },
+  {
+    ...testPayment2,
+    ...addPayment2,
+    application: testApplication,
+  },
+  {
+    ...testPayment3,
+    ...addPayment3,
+    application: testApplication,
+  },
+  {
+    ...testPayment4,
+    ...addPayment4,
+    application: testApplication,
+  },
+  {
+    ...testPayment5,
+    ...addPayment5,
+    application: testApplication,
+  },
+];
