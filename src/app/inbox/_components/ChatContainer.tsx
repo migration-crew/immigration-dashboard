a76bdd63fd53f38b5fb6eeced5257b15d9d87b98
YuiKonaka <youthfuldays585@gmail.com>
2025-02-20 16:08:58 -0800

    Set the proper position of three dots

--- src/app/inbox/_components/ChatContainer.tsx
+++ src/app/inbox/_components/ChatContainer.tsx
@@ -9,13 +9,6 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { MessageType } from "@/types/Inbox/MessageType";
@@ -47,65 +40,29 @@ export default function ChatContainer({ message }: Props) {
         className={`max-w-[574px] m-1 p-[10px] w-fit break-words ${cardRight} `}
       >
         <Caption>{message.content}</Caption>
-        <Microtext className="flex justify-end h-6">
-          {message.createdAt}
-        </Microtext>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            {/* <Button variant="outline">Open</Button> */}
-            <EllipsisVertical />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                Profile
-                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Billing
-                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Settings
-                <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Keyboard shortcuts
-                <DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuSub>
-                <DropdownMenuSubTrigger>Invite users</DropdownMenuSubTrigger>
-                <DropdownMenuPortal>
-                  <DropdownMenuSubContent>
-                    <DropdownMenuItem>Email</DropdownMenuItem>
-                    <DropdownMenuItem>Message</DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>More...</DropdownMenuItem>
-                  </DropdownMenuSubContent>
-                </DropdownMenuPortal>
-              </DropdownMenuSub>
-              <DropdownMenuItem>
-                New Team
-                <DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>GitHub</DropdownMenuItem>
-            <DropdownMenuItem>Support</DropdownMenuItem>
-            <DropdownMenuItem disabled>API</DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              Log out
-              <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex justify-end h-6">
+          <Microtext className="">{message.createdAt}</Microtext>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              {/* <Button variant="outline">Open</Button> */}
+              <EllipsisVertical />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="">
+              {/* <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator /> */}
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  Delete
+                  {/* <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut> */}
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  Edit
+                  {/* <DropdownMenuShortcut>⌘B</DropdownMenuShortcut> */}
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </Card>
     </div>
   );
