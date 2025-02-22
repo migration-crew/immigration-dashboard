4b23de18b234bd653f5865348740ad4af6612b7b
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-02-21 17:10:18 -0800

    update getApplicationTask hooks with error and loading

--- src/hooks/getApplicationTasks.ts
+++ src/hooks/getApplicationTasks.ts
@@ -5,27 +5,38 @@ export async function fetchApplicationTasks(applicationId: string) {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     const { getToken } = await auth();
     const token = await getToken();
+
+    let errorMessage = null
+    let loading = true
+    let applicationTaskSteps = null
   
     if (!token) {
       throw new Error("Failed to get authentication token");
     }
-  
-    const response = await fetch(
-      `${apiUrl}/applications/tasks?applicationId=${applicationId}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-  
-    if (!response.ok) {
-      throw new Error("Failed to fetch application tasks");
-    }
-  
-    const data = await response.json() as ApplicationTaskStageType[];
 
-    return data
+    try{
+      const response = await fetch(
+        `${apiUrl}/applications/tasks?applicationId=${applicationId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    
+      if (!response.ok) {
+        throw new Error("Failed to fetch application tasks");
+      }
+    
+      applicationTaskSteps = await response.json() as ApplicationTaskStageType[];
+    } catch (error) {
+      console.log(`Failed to fetch application tasks: ${error}`);
+      errorMessage = error as Error
+    } finally {
+      loading = false
+    }
+    console.log(applicationTaskSteps);
+    return {applicationTaskSteps, loading, errorMessage}
   }
