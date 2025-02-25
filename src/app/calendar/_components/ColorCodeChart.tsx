3e495726c7aa3598dcae369113ed844c45d3f2b0
David Torrez <dtorrez1996@gmail.com>
2025-02-24 20:42:45 -0500

    Create type and ColorCodeChart component

---
+++ src/app/calendar/_components/ColorCodeChart.tsx
@@ -0,0 +1,24 @@
+import { Card } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { ColorCode } from '@/types/Calendar/ColorCodeType';
+
+type Props = {
+  colorCodes: ColorCode[];
+};
+
+export default function ColorCodeChart({ colorCodes }: Props) {
+  return (
+    <Card className={cn('rounded-lg bg-white p-4')}>
+      <ul className='list-none space-y-2'>
+        {colorCodes.map((colorCode, index) => (
+          <li key={index} className='flex gap-2 items-center'>
+            <div
+              className={`w-4 h-4 rounded-full ${colorCode.colorClass}`}
+            ></div>
+            <span>{colorCode.description}</span>
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+}
