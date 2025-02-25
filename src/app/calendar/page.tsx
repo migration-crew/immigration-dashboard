7f02bcedc3a4b03f1cbd5814c756ad60d0a08d0b
David Torrez <dtorrez1996@gmail.com>
2025-02-24 20:45:52 -0500

    Import component in CalendarPage

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,105 +1,119 @@
-import { Button } from "@/components/ui/upImmigrationButton";
-import { DetailedCalendar } from "../calendar/_components/DetailedCalendar";
-import Link from "next/link";
-import { PageContainer } from "@/components/common/PageContainer";
-import { BreadcrumbComponent } from "@/components/common/Breadcrumbs/BreadcrumbComponent";
-import { AppointmentCard } from "@/components/common/AppointmentCard";
-import { AppointmentType } from "@/types/Appointment/AppointmentType";
-import { DynamicRoundedContainer } from "@/components/common/DynamicRoundedContainer";
+import { AppointmentCard } from '@/components/common/AppointmentCard';
+import { BreadcrumbComponent } from '@/components/common/Breadcrumbs/BreadcrumbComponent';
+import { DynamicRoundedContainer } from '@/components/common/DynamicRoundedContainer';
+import { PageContainer } from '@/components/common/PageContainer';
+import { Button } from '@/components/ui/upImmigrationButton';
+import { AppointmentType } from '@/types/Appointment/AppointmentType';
+import Link from 'next/link';
+import { DetailedCalendar } from '../calendar/_components/DetailedCalendar';
+import ColorCodeChart from './_components/ColorCodeChart';
 
 export default function CalendarPage() {
-    const links = [
-        { name: "Calendar", href: "/calendar" }
-    ];
+  const links = [{ name: 'Calendar', href: '/calendar' }];
 
-    const appointments: AppointmentType[] = [
-        {
-            id: "1",
-            appointmentType: {
-                id: "1",
-                name: "Zoom Meeting",
-                duration: 60,
-                currency: "USD",
-                price: 100,
-            },
-            date: new Date("2024-01-28T16:00:00"),
-            user: {
-                id: "1",
-                firstName: "John",
-                lastName: "Doe",
-                nationality: "US",
-                language: "English",
-                address: "123 Main St, Anytown, USA",
-                birthDate: "1990-01-01",
-                gender: "Male",
-                email: "john.doe@example.com",
-                imageUrl: "/placeholder.svg",
-            },
-        },
-        {
-            id: "2",
-            appointmentType: {
-                id: "1",
-                name: "First Consultation",
-                duration: 60,
-                currency: "USD",
-                price: 100,
-            },
-            date: new Date("2024-01-28T16:00:00"),
-            user: {
-                id: "1",
-                firstName: "John",
-                lastName: "Doe",
-                nationality: "US",
-                language: "English",
-                address: "123 Main St, Anytown, USA",
-                birthDate: "1990-01-01",
-                gender: "Male",
-                email: "john.doe@example.com",
-                imageUrl: "/placeholder.svg",
-            },
-        },
-        {
-            id: "3",
-            appointmentType: {
-                id: "1",
-                name: "First Consultation",
-                duration: 60,
-                currency: "USD",
-                price: 100,
-            },
-            date: new Date("2024-01-28T16:00:00"),
-            user: {
-                id: "1",
-                firstName: "John",
-                lastName: "Doe",
-                nationality: "US",
-                language: "English",
-                address: "123 Main St, Anytown, USA",
-                birthDate: "1990-01-01",
-                gender: "Male",
-                email: "john.doe@example.com",
-                imageUrl: "/placeholder.svg",
-            },
-        }
-    ]
+  const appointments: AppointmentType[] = [
+    {
+      id: '1',
+      appointmentType: {
+        id: '1',
+        name: 'Zoom Meeting',
+        duration: 60,
+        currency: 'USD',
+        price: 100,
+      },
+      date: new Date('2024-01-28T16:00:00'),
+      user: {
+        id: '1',
+        firstName: 'John',
+        lastName: 'Doe',
+        nationality: 'US',
+        language: 'English',
+        address: '123 Main St, Anytown, USA',
+        birthDate: '1990-01-01',
+        gender: 'Male',
+        email: 'john.doe@example.com',
+        imageUrl: '/placeholder.svg',
+      },
+    },
+    {
+      id: '2',
+      appointmentType: {
+        id: '1',
+        name: 'First Consultation',
+        duration: 60,
+        currency: 'USD',
+        price: 100,
+      },
+      date: new Date('2024-01-28T16:00:00'),
+      user: {
+        id: '1',
+        firstName: 'John',
+        lastName: 'Doe',
+        nationality: 'US',
+        language: 'English',
+        address: '123 Main St, Anytown, USA',
+        birthDate: '1990-01-01',
+        gender: 'Male',
+        email: 'john.doe@example.com',
+        imageUrl: '/placeholder.svg',
+      },
+    },
+    {
+      id: '3',
+      appointmentType: {
+        id: '1',
+        name: 'First Consultation',
+        duration: 60,
+        currency: 'USD',
+        price: 100,
+      },
+      date: new Date('2024-01-28T16:00:00'),
+      user: {
+        id: '1',
+        firstName: 'John',
+        lastName: 'Doe',
+        nationality: 'US',
+        language: 'English',
+        address: '123 Main St, Anytown, USA',
+        birthDate: '1990-01-01',
+        gender: 'Male',
+        email: 'john.doe@example.com',
+        imageUrl: '/placeholder.svg',
+      },
+    },
+  ];
 
-    return (
-        <>
-            <PageContainer>
-                <BreadcrumbComponent links={links} />
-                <div>
-                    <Button asChild><Link href={"/appointment"}>appointment</Link></Button>
-                </div>
-                <div className="flex gap-4 justify-between">
-                    <DynamicRoundedContainer title="Upcoming Appointments" className="w-[350px] h-[850px]">
-                        {appointments.map((appointment) => (
-                            <AppointmentCard key={appointment.id} appointment={appointment} attendees={[]} />
-                        ))}
-                    </DynamicRoundedContainer>
-                    <DetailedCalendar />
-                </div>
-            </PageContainer>
-        </>
-    )
+  return (
+    <>
+      <PageContainer>
+        <BreadcrumbComponent links={links} />
+        <div className='flex justify-between items-center'>
+          <Button asChild>
+            <Link href={'/appointment'}>appointment</Link>
+          </Button>
+          <ColorCodeChart
+            colorCodes={[
+              { colorClass: 'bg-secondary-blue', description: 'Appointment' },
+              { colorClass: 'bg-primary-red', description: 'Tasks' },
+            ]}
+          />
+        </div>
+        <div className='flex gap-4 justify-between'>
+          <DynamicRoundedContainer
+            title='Upcoming Appointments'
+            className='w-[350px] h-[850px]'
+          >
+            {appointments.map((appointment) => (
+              <AppointmentCard
+                key={appointment.id}
+                appointment={appointment}
+                attendees={[]}
+              />
+            ))}
+          </DynamicRoundedContainer>
+          <DetailedCalendar />
+        </div>
+      </PageContainer>
+    </>
+  );
 }
