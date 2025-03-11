faaf1423e4b271dc1462d5cd72ee6930eefb3dae
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-03-11 12:53:38 -0700

    implement calendar api

--- src/app/api/services/calendar/calendarService.ts
+++ src/app/api/services/calendar/calendarService.ts
@@ -1,14 +1,19 @@
+import { toDate } from "date-fns";
 import { getUserId } from "../../lib/getUserId";
 import dbConnect from "../../lib/mongoose";
 import Calendar from "../../schemas/calendar/calendar.schema";
+import { log } from "console";
 
 
-export const getAllEvents = async (userId: string) => {
+export const getAllEvents = async (userId: string, currentMonth: Date, nextMonth: Date) => {
   await dbConnect();
   const userIdFromDB = await getUserId(userId);
   console.log("🚀", userIdFromDB);
+  console.log("🚀", currentMonth);
+  console.log("🚀", nextMonth);
+  
 
-  const events = await Calendar.find({ user: userIdFromDB }).select("-user")
+  const events = await Calendar.find({ user: userIdFromDB, start: {"$gte": currentMonth, "$lte": nextMonth} }).select("-user")
 
   return events;
 };
