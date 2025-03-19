151c903018f78a88d9019ee33365b28effe4c316
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-03-19 15:38:57 -0700

    adopt fronend to hook

--- src/hooks/getAllEvents.ts
+++ src/hooks/getAllEvents.ts
@@ -1,27 +1,33 @@
-import { auth } from "@clerk/nextjs/server";
+import { EventType } from "@/types/Calendar/EventType";
 
-export const getAllEvents = async (year: number, month: number) => {
-  const { getToken } = await auth();
+export const getAllEvents = async (
+  token: string | null,
+  year: string,
+  month: number
+) => {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-  const token = await getToken();
-  console.log(token);
+
   if (!token) {
     throw new Error("Failed to get authentication token");
   }
   try {
-    const response = await fetch(`${apiUrl}/events?month=${month}&year=${year}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    console.log("fetching events");
+    const response = await fetch(
+      `${apiUrl}/events?month=${month}&year=${year}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     if (!response.ok) {
       throw new Error("Failed to fetch events");
     }
-    return await response.json();
+    return (await response.json()) as EventType[];
   } catch (error) {
     console.log("😮", "failed to fetch events: ", error);
-    return error;
+    return null;
   }
-}
+};
