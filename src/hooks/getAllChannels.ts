429af53dcaa8323e22fa059b2ebafd5cdad0173f
SauloAC <saulo.andrade.costa@gmail.com>
2025-03-25 15:00:09 -0700

    Create Get all Channel

--- src/hooks/getAllChannels.ts
+++ src/hooks/getAllChannels.ts
@@ -1,13 +1,13 @@
-import { MessageType } from "@/types/Inbox/MessageType";
+import { ChannelType } from "@/types/Inbox/ChannelType";
 import { auth } from "@clerk/nextjs/server";
 
-export const getAllMessages = async (channelId: string) => {
+export const getAllChannels = async () => {
   const { getToken } = await auth();
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   const token = await getToken();
 
   try {
-    const response = await fetch(`${apiUrl}/inbox/${channelId}/message`, {
+    const response = await fetch(`${apiUrl}/inbox`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -17,7 +17,7 @@ export const getAllMessages = async (channelId: string) => {
     if (!response.ok) {
       throw new Error("Failed to fetch messages");
     }
-    return response.json() as unknown as MessageType[];
+    return response.json() as unknown as ChannelType[];
   } catch (error) {
     console.log("😮", "failed to fetch messages: ", error);
     return [];
