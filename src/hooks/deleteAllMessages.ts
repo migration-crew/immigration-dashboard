cac9c0758c3252d8de86066c11373bcf9c83ffe9
YuiKonaka <youthfuldays585@gmail.com>
2025-03-26 17:45:04 -0700

    Put Basic hooks code

---
+++ src/hooks/deleteAllMessages.ts
@@ -0,0 +1,25 @@
+import { MessageType } from "@/types/Inbox/MessageType";
+import { auth } from "@clerk/nextjs/server";
+
+export const deleteAllMessages = async (channelId: string) => {
+  const { getToken } = await auth();
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const token = await getToken();
+
+  try {
+    const response = await fetch(`${apiUrl}/inbox/${channelId}/message`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Failed to fetch messages");
+    }
+    return response.json() as unknown as MessageType[];
+  } catch (error) {
+    console.log("😮", "failed to fetch messages: ", error);
+    return [];
+  }
+};
