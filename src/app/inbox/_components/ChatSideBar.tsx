006025ea8768de187425709ddaa22122f49a97a6
David Torrez <dtorrez1996@gmail.com>
2025-03-31 20:55:22 -0400

    Update to double quotation in ChatSideBar.tsx

--- src/app/inbox/_components/ChatSideBar.tsx
+++ src/app/inbox/_components/ChatSideBar.tsx
@@ -1,20 +1,20 @@
-'use client';
+"use client";
 
-import { CaptionSemi } from '@/components/common/text/CaptionSemi';
-import { Card } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/upImmigrationButton';
-import { ChannelType } from '@/types/Inbox/ChannelType';
-import { Search } from 'lucide-react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { CaptionSemi } from "@/components/common/text/CaptionSemi";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/upImmigrationButton";
+import { ChannelType } from "@/types/Inbox/ChannelType";
+import { Search } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 type Props = {
   chats: ChannelType[];
 };
 
 export default function ChatSideBar({ chats }: Props) {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   const searchParams = useSearchParams();
   const pathname = usePathname();
@@ -22,11 +22,11 @@ export default function ChatSideBar({ chats }: Props) {
   // const [value, setValue] = useState(chats[0]._id);
 
   useEffect(() => {
-    const currentMessageId = searchParams.get('messageId');
+    const currentMessageId = searchParams.get("messageId");
     if (!currentMessageId) {
       const params = new URLSearchParams(searchParams);
       params.append(
-        'messageId',
+        "messageId",
         chats.find((chat) => chat.users.length > 2)?._id || chats[0]._id
       );
       replace(`${pathname}?${params.toString()}`);
@@ -36,9 +36,9 @@ export default function ChatSideBar({ chats }: Props) {
   const onMessageChange = (value: string | null) => {
     // setValue(value || chats[0]._id);
     const params = new URLSearchParams(searchParams);
-    params.delete('messageId');
+    params.delete("messageId");
     // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-    params.append('messageId', value || chats[0]._id);
+    params.append("messageId", value || chats[0]._id);
     replace(`${pathname}?${params.toString()}`);
   };
 
@@ -61,30 +61,30 @@ export default function ChatSideBar({ chats }: Props) {
   );
 
   const currentChannelDesign = (channelId: string) => {
-    return channelId === searchParams.get('messageId')
-      ? 'bg-secondary-green text-primary-white'
-      : 'text-black bg-inherit hover:bg-primary-gray hover:text-primary-black';
+    return channelId === searchParams.get("messageId")
+      ? "bg-secondary-green text-primary-white"
+      : "text-black bg-inherit hover:bg-primary-gray hover:text-primary-black";
   };
 
   return (
-    <div className='w-[290px] h-full'>
-      <Card className='flex flex-col gap-6 py-10 px-5 h-[840px]'>
-        <form className='relative w-full max-w-md mx-auto'>
+    <div className="w-[290px] h-full">
+      <Card className="flex flex-col gap-6 py-10 px-5 h-[840px]">
+        <form className="relative w-full max-w-md mx-auto">
           <Input
-            type='text'
-            placeholder='Search Users'
+            type="text"
+            placeholder="Search Users"
             value={searchText}
             onChange={(e) => setSearchText(e.target.value)}
-            className='pl-10 pr-4 py-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2'
+            className="pl-10 pr-4 py-2 w-full rounded-lg border border-gray-300 focus:outline-none focus:ring-2"
           />
-          <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5' />
-          <button type='submit' className='sr-only'>
+          <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-5 w-5" />
+          <button type="submit" className="sr-only">
             Search
           </button>
         </form>
-        <div className='flex flex-col gap-6'>
+        <div className="flex flex-col gap-6">
           <CaptionSemi>Channel</CaptionSemi>
-          <div className='flex flex-col gap-2 w-[250px]'>
+          <div className="flex flex-col gap-2 w-[250px]">
             {filteredChannels.map((channel, index) => (
               <Button
                 key={index}
@@ -101,9 +101,9 @@ export default function ChatSideBar({ chats }: Props) {
             ))}
           </div>
         </div>
-        <div className='flex flex-col gap-6'>
+        <div className="flex flex-col gap-6">
           <CaptionSemi>Direct Message</CaptionSemi>
-          <div className='flex flex-col gap-2'>
+          <div className="flex flex-col gap-2">
             {filteredMessages.map((message, index) => (
               <Button
                 key={index}
