49ba46a3cf3f8c465e1d62bc6198aecabde10b13
Yuki Higuchi <129672772+reiz0@users.noreply.github.com>
2025-04-25 16:32:04 -0700

    delete unnesessary Props and change function type

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -14,11 +14,11 @@ import { useAuth } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import ProfileInput from "./_components/ProfileInput";
 
-export type Props = {
-  profile: UserType[];
-};
+// export type Props = {
+//   profile: UserType[];
+// };
 
-export const page = ({}: Props) => {
+export default function ProfilePage () {
   const links = [{ name: "Profile", href: "/profile" }];
   const [users, setUsers] = useState<UserType | null>(null);
   const [imageURL, setImageURL] = useState<string | null>(null);
@@ -130,5 +130,3 @@ export const page = ({}: Props) => {
     </PageContainer>
   );
 };
-
-export default page;
